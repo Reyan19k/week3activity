38d53bbc60524aed7e913445ede21db60b6fb769
Reyan <dinreyanud@gmail.com>
2022-09-13 10:08:46 +0100

    first commit

--- activity.js
+++ activity.js
@@ -1,8 +1,8 @@
 const displayKey = document.querySelector(".key h2");
-const displayKeyode = document.querySelector(".keyCode h2");
-const keyodeDiv = document.querySelector(".keyCode");
+const displayKeyCode = document.querySelector(".keyCode h2");
+const keyCodeDiv = document.querySelector(".keyCode");
 
 window.addEventListener("keydown", (i) =>{
     displayKey.innerText = i.key;
-    displayKeyode.innerText = i.keyCode;
+    displayKeyCode.innerText = i.keyCode;
 })
